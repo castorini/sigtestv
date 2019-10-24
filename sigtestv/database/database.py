7e6fdeda78b20581ff352f759a5cfbc3c05278ee
Ralph Tang <r33tang@uwaterloo.ca>
2019-10-24 14:59:41 -0400

    Add database connection context manager

--- sigtestv/database/database.py
+++ sigtestv/database/database.py
@@ -6,13 +6,36 @@ import sqlite3
 from sigtestv.evaluate import ExperimentResult, RunConfiguration, PipelineComponent, RunCollection
 
 
+class ConnectionContextManager(object):
+
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.opened = 0
+
+    def __enter__(self):
+        if self.opened == 0:
+            self.connection = sqlite3.connect(self.filename)
+            self.cursor = self.connection.cursor()
+        self.opened += 1
+        return self
+
+    def __exit__(self, *args):
+        self.opened -= 1
+        if self.opened == 0:
+            self.connection.commit()
+            self.connection.close()
+
+
+def open_context(filename):
+    return ConnectionContextManager(filename)
+
+
 class ResultsDatabase(object):
 
     def __init__(self, filename):
         self.filename = filename
-        conn = sqlite3.connect(filename)
-        with conn:
-            c = conn.cursor()
+        with open_context(filename) as ctx:
+            c = ctx.cursor
             c.execute('''CREATE TABLE IF NOT EXISTS model(id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE)''')
             c.execute('''CREATE INDEX IF NOT EXISTS model_name_i ON model(name)''')
             c.execute('''CREATE TABLE IF NOT EXISTS experiment(id INTEGER PRIMARY KEY, model_id INTEGER NOT NULL,
@@ -26,10 +49,8 @@ class ResultsDatabase(object):
             c.execute('''CREATE TABLE IF NOT EXISTS experiment_hyperparameter_assoc(id INTEGER PRIMARY KEY, exp_id INTEGER NOT NULL,
                          name TEXT NOT NULL, value TEXT, FOREIGN KEY(exp_id) REFERENCES experiment(id) ON DELETE CASCADE)''')
             c.execute('''CREATE INDEX IF NOT EXISTS experiment_hyperparameter_assoc_exp_id_fi ON experiment_hyperparameter_assoc(exp_id)''')
-            conn.commit()
 
     def fetch_all(self, model_name, dataset_name):
-        conn = sqlite3.connect(self.filename)
         query = '''SELECT * FROM MODEL JOIN experiment ON model.id=model_id JOIN experiment_result_assoc ON 
                    experiment_result_assoc.exp_id=experiment.id JOIN experiment_hyperparameter_assoc ON 
                    experiment.id=experiment_hyperparameter_assoc.exp_id WHERE model.name=? AND dataset_name=?'''
@@ -37,8 +58,8 @@ class ResultsDatabase(object):
         exp_metadata = {}
         exp_opts = defaultdict(list)
         exp_results = defaultdict(list)
-        with conn:
-            c = conn.cursor()
+        with open_context(self.filename) as ctx:
+            c = ctx.cursor
             c.execute(query, data)
             for row in c.fetchall():
                 model_id, model_name, exp_id, model_id, timestamp, cmd_txt, ds_name, exp_id, exp_id, measure_name, \
@@ -54,7 +75,18 @@ class ResultsDatabase(object):
             env_vars = dict(filter(lambda x: not x[0].startswith('-'), set(exp_opts[exp_id])))
             configs.append(RunConfiguration(model_name, cmd_txt, ds_name, options, env_vars))
             results.append([ExperimentResult(*x) for x in set(exp_results[exp_id])])
-        return RunCollection(configs, results)
+        return RunCollection(configs, results, metadata=[dict(exp_id=exp_id) for exp_id, _ in exp_metadata])
+
+    def add_result(self, exp_id, results: List[ExperimentResult], ctx=None):
+        if ctx is None: ctx = open_context(self.filename)
+        with ctx:
+            c = ctx.cursor
+            query = 'INSERT INTO experiment_result_assoc(exp_id, metric_name, result, set_type) VALUES (?, ?, ?, ?)'
+            values = []
+            for result in results:
+                values.append((exp_id, result.name, result.value, result.set_type))
+            for value in values:
+                c.execute(query, value)
 
     def insert_result(self,
                       model_name: str,
@@ -62,9 +94,8 @@ class ResultsDatabase(object):
                       full_command_str: str,
                       results: List[ExperimentResult],
                       hyperparameters: List[Tuple[str, str]]):
-        conn = sqlite3.connect(self.filename)
-        with conn:
-            c = conn.cursor()
+        with open_context(self.filename) as ctx:
+            c = ctx.cursor
             c.execute('SELECT id FROM model WHERE name=?', (model_name,))
             row = c.fetchone()
             if row:
@@ -75,26 +106,19 @@ class ResultsDatabase(object):
 
             c.execute('INSERT INTO experiment(model_id, command_text, dataset_name) VALUES (?, ?, ?)', (model_id, full_command_str, dataset_name))
             exp_id = c.lastrowid
-
-            values = []
-            for result in results:
-                values.append((exp_id, result.name, result.value, result.set_type))
-            query = f'INSERT INTO experiment_result_assoc(exp_id, metric_name, result, set_type) VALUES (?, ?, ?, ?)'
-            for value in values:
-                c.execute(query, value)
+            self.add_result(exp_id, results, ctx=ctx)
 
             values = [(exp_id, x[0], x[1]) for x in hyperparameters]
             query = f'INSERT INTO experiment_hyperparameter_assoc(exp_id, name, value) VALUES (?, ?, ?)'
             for value in values:
                 c.execute(query, value)
-            conn.commit()
 
 
 @dataclass
 class DatabaseLogger(PipelineComponent):
     database: ResultsDatabase
 
-    def __call__(self, run_config: RunConfiguration, results: List[ExperimentResult]):
+    def __call__(self, run_config: RunConfiguration, results: List[ExperimentResult], **kwargs):
         self.database.insert_result(run_config.model_name,
                                     run_config.dataset_name,
                                     run_config.command_base,
