c8349342a025f850fe131ab51ee63ee180cd427c
Ralph Tang <r33tang@uwaterloo.ca>
2019-11-16 21:01:19 -0500

    Add ECDF CI band computation

--- sigtestv/stats/ci.py
+++ sigtestv/stats/ci.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .utils import ecdf
+
 
 def bootstrap_ci(sample: np.ndarray,
                  estimate_fn,
@@ -16,3 +18,11 @@ def bootstrap_ci(sample: np.ndarray,
         qa1 = 2 * est - qa2
         qa2 = 2 * est - tmp
     return est, (qa1, qa2)
+
+
+def compute_ecdf_ci_bands(sample: np.ndarray, alpha, k=1, **kwargs):
+    n = len(sample)
+    eps = np.sqrt(np.log(2 / alpha) / (2 * n)) ** k
+    cdf, sample = ecdf(sample, **kwargs)
+    cdf = cdf ** k
+    return (np.clip(cdf - eps, 0, 1), np.clip(cdf + eps, 0, 1)), sample
